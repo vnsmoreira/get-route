1ed6307ade554f7333d1371aecaa9ac324a3cbc2
Vinícius <vnsmoreira.dev@gmail.com>
2022-04-20 02:39:00 -0300

    update: load distance routes after cluster is initialized

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { initializeCluster } = require('./config/puppeteer');
+const { onClusterInitialize } = require('./config/puppeteer');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-initializeCluster().then(() => {
+onClusterInitialize(() => {
   require('./app/routes/distance.routes')(app);
 });
 
