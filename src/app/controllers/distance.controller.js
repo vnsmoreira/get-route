f0f50c01f4de948686150c92fab778625ef91628
Vinícius <vnsmoreira.dev@gmail.com>
2022-04-28 02:20:02 -0300

    update: add mode (driving or walking) option

--- src/app/controllers/distance.controller.js
+++ src/app/controllers/distance.controller.js
@@ -1,11 +1,12 @@
-const controller = {};
 const { getCluster } = require('../../config/puppeteer');
+
+const controller = {};
 
 const cluster = getCluster();
 
 controller.getDistance_POST = async (req, res) => {
   try {
-    const { addresses } = req.body;
+    const { addresses, mode } = req.body;
 
     const isNotArrayOfStrings = addresses
       .map(address => typeof address)
@@ -15,7 +16,13 @@ controller.getDistance_POST = async (req, res) => {
       return res.status(400).send({ error: '"addresses" must be an array of strings' });
     }
 
-    let response = await cluster.execute({ addresses });
+    if (mode !== 'driving' && mode !== 'walking' && mode !== undefined) {
+      return res
+        .status(400)
+        .send({ error: '"mode" option should be either "driving" or "walking"' });
+    }
+
+    let response = await cluster.execute({ addresses, mode });
 
     return res.send(response);
   } catch (err) {
@@ -26,8 +33,25 @@ controller.getDistance_POST = async (req, res) => {
 controller.getDistance_GET = async (req, res) => {
   try {
     let { addressA, addressB } = req.params;
+    const { mode } = req.query;
 
-    let response = await cluster.execute({ addresses: [addressA, addressB] });
+    const addresses = [addressA, addressB];
+
+    const isNotArrayOfStrings = addresses
+      .map(address => typeof address)
+      .some(type => type !== 'string');
+
+    if (!Array.isArray(addresses) || isNotArrayOfStrings) {
+      return res.status(400).send({ error: '"addresses" must be an array of strings' });
+    }
+
+    if (mode !== 'driving' && mode !== 'walking' && mode !== undefined) {
+      return res
+        .status(400)
+        .send({ error: '"mode" option should be either "driving" or "walking"' });
+    }
+
+    let response = await cluster.execute({ addresses, mode });
 
     return res.send(response);
   } catch (err) {
