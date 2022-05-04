b262f1974432aca57e1b4501a0862a065667bc9c
Vinícius <vnsmoreira.me@gmail.com>
2022-05-04 11:52:04 -0300

    update: limit requests per user

---
+++ src/config/rate-limit/index.js
@@ -0,0 +1,10 @@
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = limiter;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { onClusterInitialize } = require('./config/puppeteer');
+const limiter = require('./config/rate-limit');
 
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,4 +18,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.info(`Running on port:${port}`);
 });
-
