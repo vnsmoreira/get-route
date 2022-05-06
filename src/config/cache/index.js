9cdafd453274987436ff8b8652720a4da28ddba8
Vinícius <vnsmoreira.dev@gmail.com>
2022-05-05 23:59:16 -0300

    update: install ioredis and create cache module

---
+++ src/config/cache/index.js
@@ -0,0 +1,16 @@
+const Redis = require('ioredis');
+const redis = new Redis();
+
+const cache = {};
+
+cache.set = (key, value, expirationMs) => {
+  return redis.set(key, JSON.stringify(value), 'EX', expirationMs);
+};
+
+cache.get = async key => {
+  const value = await redis.get(key);
+
+  return value ? JSON.parse(value) : null;
+};
+
+module.exports = cache;
