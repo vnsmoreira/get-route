5bb9d6703a4c88d65d7442618120ada5c8f6e09a
Vinícius <vnsmoreira.dev@gmail.com>
2022-12-22 21:06:48 -0300

    fix: change default application port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import { app } from './app';
 import scraper from '#puppeteer';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const startServer = () => app.listen(port);
 
