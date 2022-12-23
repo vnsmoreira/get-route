be6412b665e73a742fc7d6846de934d662b6c63b
Vinícius <vnsmoreira.dev@gmail.com>
2022-12-23 18:44:50 -0300

    feat: add database implementation

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true);
+
+async function onMongoConnect() {
+  const mongoDBUri = process.env.MONGO_DB_URI;
+
+  try {
+    await mongoose.connect(mongoDBUri);
+
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false };
+  }
+}
+
+export default onMongoConnect;
