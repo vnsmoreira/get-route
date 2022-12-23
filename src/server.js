04debe2b9b63ca901b192209327314799aee54ea
Vinícius <vnsmoreira.dev@gmail.com>
2022-12-23 16:43:40 -0300

    feat: change default application port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import { app } from './app';
 import scraper from '#puppeteer';
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 const startServer = () => app.listen(port);
 
